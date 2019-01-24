7ae4151151e33c4ef76d74badcb56758e5b17840
Luiz Silva <luiz.silva@concrete.com.br>
2019-01-24 19:07:53 -0200

    Adds template for QuickNimble tests

---
+++ QuickNimble Test Case.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,31 @@
+//___FILEHEADER___
+
+import Quick
+import Nimble
+@testable import ___PROJECTNAME___
+
+class ___FILEBASENAMEASIDENTIFIER___Test: QuickSpec {
+
+    override func spec() {
+
+        describe("___FILEBASENAMEASIDENTIFIER___") {
+
+            beforeEach {
+                <#Setup test fixtures here#>
+            }
+
+            context("<#context name goes here#>") {
+
+                it("<#test predicate goes here#>") {
+
+                    <#do test assertions with expect() here#>
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
